804a7df89bfa396d5abbab91527c75934c7305dd
Thiago Cruz dos Santos <thgsantos16@gmail.com>
2016-10-27 10:21:29 -0200

    Primeira versão Alfa 1.0

--- conexao_fluig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_server = '10.1.10.130\Producao';
-$db_database = 'FLUIG';
-$db_user = 'ecm';
-$db_passwd = 'T0tv5Flu1g';
-
-$connectionInfo = array("Database"=>$db_database, "UID"=>$db_user, "PWD"=>$db_passwd, "CharacterSet" => "UTF-8");
-$con3 = sqlsrv_connect($db_server, $connectionInfo);
